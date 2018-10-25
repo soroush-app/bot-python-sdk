88a04c3bb2254d35b6ba3dfdc942ef20f27adcc4
علی نیک نشان <nikneshan@baregheh.com>
2018-10-25 16:06:55 +0330

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/alinik/soroush_python_sdk',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )

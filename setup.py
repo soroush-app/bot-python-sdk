f0baa2f30d0f950f14c5cc0a6509e152cd361c3c
علی نیک نشان <nikneshan@baregheh.com>
2018-10-25 14:01:57 +0330

    Bump version: 0.2.1 → 0.2.1

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/alinik/soroush_python_sdk',
-    version='version='0.2.1'',
+    version='version='version='0.2.1''',
     zip_safe=False,
 )

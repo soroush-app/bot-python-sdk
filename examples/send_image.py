7a45e1985c1f8b892226398bd11c56bfa1358129
Mahdiali313 <52355596+Mahdiali313@users.noreply.github.com>
2021-04-25 21:28:12 +0430

    Update send_image.py

--- examples/send_image.py
+++ examples/send_image.py
@@ -9,7 +9,7 @@ to = 'user chat_id'
 path_image = 'image path'
 path_image_thumbnail = 'thumbnail path'
 
-[error, success] = bot.sendVideo(
+[error, success] = bot.sendImage(
                         target_id = to,
                         image_path = path_image,
                         image_thumbnail_path = path_image_thumbnail ,
